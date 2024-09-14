1b1f0ee789988dbadf7dbc81e021cc13a682f510
highambition211 <highambition211@gmail.com>
2024-09-14 11:47:28 +0200

    fix: modify structure

--- src/locationBasedScraper.ts
+++ src/locationBasedScraper.ts
@@ -4,6 +4,7 @@ const API_KEY = process.env.API_KEY ?? 'YOUR_DEFAULT_API_KEY' // <--- Enter your
 
 const locationBasedScraper = async (location: string) => {
     const OTM_PAGE_URL = "https://www.onthemarket.com/for-sale/property/" + location + "/?view=map-list"
+    console.log("onthemarketPropertyScraper - API_KEY : ", API_KEY)
     const properties = await onthemarketPropertyScraper(API_KEY, OTM_PAGE_URL)
     return properties
 }
