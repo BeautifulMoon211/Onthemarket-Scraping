4a1af47699287c614bdef950ceb0d000fa5eb6f0
beautifulmoon211 <sweemmizuki211@gmail.com>
2024-09-20 13:38:13 +0200

    fix: remove env package

--- src/locationBasedScraper.ts
+++ src/locationBasedScraper.ts
@@ -1,7 +1,5 @@
 import onthemarketPropertyScraper from './onthemarketPropertyScraper.js'
 
-const API_KEY = process.env.API_KEY ?? 'YOUR_DEFAULT_API_KEY'
-
 const locationBasedScraper = async (location: string) => {
     await onthemarketPropertyScraper(location)
 }
