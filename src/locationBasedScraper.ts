fd42c49a52ba9427b404673ef9300d0bb74f6875
beautifulmoon211 <sweemmizuki211@gmail.com>
2024-09-22 14:35:08 -0700

    feat: add auxiliary files

--- src/locationBasedScraper.ts
+++
@@ -1,7 +0,0 @@
-import onthemarketPropertyScraper from './onthemarketPropertyScraper.js'
-
-const locationBasedScraper = async (location: string) => {
-    await onthemarketPropertyScraper(location)
-}
-
-export default locationBasedScraper
